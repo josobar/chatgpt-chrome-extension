8163cde9a956ed82c3df12d6db88e5c7cd7bba10
Jason Barnett <jasoneb3@illinois.edu>
2023-02-25 00:22:50 -0600

    completed /generate endpoint

--- server/pages/app.js
+++ server/pages/app.js
@@ -1,9 +1,13 @@
 import { Configuration, OpenAIApi } from "openai";
 import express, { json } from "express";
+import {} from "dotenv/config";
 
 const app = express();
 app.use(express.json());
-const port = 3000;
+
+const PORT = 3000;
+const COMPLETION_TEMPERATURE = 0.6;
+const MAX_TOKENS = 1000;
 
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
@@ -15,12 +19,31 @@ app.get("/", (req, res) => {
   res.sendfile("pages/image (1).png");
 });
 
-app.post("/generate", (req, res) => {
-  // console.log(req.body)
-  res.send(req.body);
-  // console.log;
+app.post("/generate", async (req, res) => {
+  const inputPrompt = req.body.prompt;
+  try {
+    const completion = await openai.createCompletion({
+      model: "text-davinci-003",
+      prompt: inputPrompt,
+      temperature: COMPLETION_TEMPERATURE,
+      max_tokens: MAX_TOKENS,
+    });
+    res.status(200).json({ result: completion.data.choices[0].text });
+  } catch (error) {
+    if (error.response) {
+      console.error(error.response.status, error.response.data);
+      res.status(error.response.status).json(error.response.data);
+    } else {
+      console.error(`Error with OpenAI API request: ${error.message}`);
+      res.status(500).json({
+        error: {
+          message: "An error occurred during your request.",
+        },
+      });
+    }
+  }
 });
 
-app.listen(port, () => {
+app.listen(PORT, () => {
   console.log("SERVER ONLINE :3");
 });
